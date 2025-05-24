a2397af8795eca7cd782bdbb43f7d25b5a676e92
Arkadiy <agongadze8@gmail.com>
2025-05-24 18:55:54 +0300

    Сущности добавлены в админку

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from app.models import Client, Message, Newsletter, Attempt
+
+
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email',)
+    search_fields = ('name', 'surname', 'email',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('date_time', 'periodicity', 'status')
+    search_fields = ('date_time', 'periodicity', 'status')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('date_time', 'status', 'response')
+    search_fields = ('date_time', 'status', 'response')
